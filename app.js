6e5f4e8df530ff97506d904c2a24bedde7bbef2c
Camila Borges <camis02121999@gmail.com>
2025-02-27 16:01:31 -0300

    criada rota para o user

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require("express");
+import express from "express";
+import { router as userRouter } from "./routes/users";
+
 const app = express();
+const port = 3000;
 
-const { PORT = 3000 } = process.env;
+function logger(req, res, next) {
+  console.log(
+    `${new Intl.DateTimeFormat("pt-BR").format(Date.now())} - ${req.method} - ${
+      req.url
+    }`
+  );
+  next();
+}
 
-app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
+app.use(logger);
+
+app.use("/users", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
